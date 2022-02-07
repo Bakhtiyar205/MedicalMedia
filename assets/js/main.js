c7eec776bdd8a0c2360e790ee5972d9ea851c2ae
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-02-07 08:32:10 +0400

    Pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,14 @@ let rightBtn = document.getElementById("right-btn");
 
 rightBtn.addEventListener("click", function () {
   let active = document.querySelectorAll(".active");
+  let card =document.querySelectorAll(".card-visual")
   if (active[3].nextElementSibling != null) {
-      leftBtn.classList.remove("disabled")
-      active[0].classList.remove("active");
-      active[3].nextElementSibling.classList.add("active");
-    //   for (let i = 0; i < active.length; i++) {
-    //       active[i].style.transform = "translateX(-200px)"
-    //   }
+    leftBtn.classList.remove("disabled")
+    active[0].classList.remove("active");
+    active[3].nextElementSibling.classList.add("active");
+    // for (let i = 0; i < card.length; i++) {
+    //     card[i].style.transform = "translateX(-270px)"
+    // }
   } else {
     rightBtn.classList.add("disabled")
   } 
@@ -21,9 +22,9 @@ leftBtn.addEventListener("click", function () {
     rightBtn.classList.remove("disabled")
     active[3].classList.remove("active");
     active[0].previousElementSibling.classList.add("active");
-    // for (let i = 0; i < active.length; i++) {
-    //     active[i].style.transform = "translateX(200px)"
-    // }
+  //   for (let i = 0; i < card.length; i++) {
+  //     card[i].style.transform = "translateX(400px)"
+  // }
   } else {
     leftBtn.classList.add("disabled")
   }
